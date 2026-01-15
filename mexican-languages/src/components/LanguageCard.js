c6cd5fe269df29395e2e3b93b6c4ff1e4462c3b2
spiderasasen <spiderasasen@gmail.com>
2026-01-15 15:52:51 -0500

    got some more system down and got the templete as i like it. it has an image and a discription(that will be changed later) plus a better system Now to get the style ready i guess

--- mexican-languages/src/components/LanguageCard.js
+++ mexican-languages/src/components/LanguageCard.js
@@ -2,8 +2,8 @@ function LanguageCard({data}) {
     return(
         <div className="language-card">
             <h3>{data.name}</h3>
-            <p><strong>Region: {data.region}</strong></p>
-            <p><strong>Status: {data.status}</strong></p>
+            <img src={data.image} alt={data.imageAlt} />
+            <p><strong>People still speaking it: </strong>{data.population}</p>
             <p>{data.description}</p>
         </div>
     )
